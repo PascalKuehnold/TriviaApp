8371c247e602a433002474504883dd666a163ee7
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-21 01:55:39 +0200

    added HiltAndroidApp

---
+++ app/src/main/java/de/pascalkuehnold/triviaapp/TriviaApplication.kt
@@ -0,0 +1,11 @@
+package de.pascalkuehnold.triviaapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by Pascal Kühnold on 7/18/2022.
+ */
+@HiltAndroidApp
+class TriviaApplication: Application() {}
