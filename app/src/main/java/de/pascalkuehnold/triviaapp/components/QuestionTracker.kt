7a85df4b310570f20a3948d4538c5f035c0cdf6a
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-21 01:55:07 +0200

    added QuestionTracker composable for tracking the current questions

---
+++ app/src/main/java/de/pascalkuehnold/triviaapp/components/QuestionTracker.kt
@@ -0,0 +1,43 @@
+package de.pascalkuehnold.triviaapp.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import de.pascalkuehnold.triviaapp.util.AppColors
+
+@Composable
+fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
+    Text(text = buildAnnotatedString {
+        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+            withStyle(
+                style = SpanStyle(
+                    color = AppColors.mLightGray,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 27.sp
+                )
+            ) {
+                append("Question $counter/")
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 14.sp
+                    )
+                ) {
+                    append("$outOf")
+                }
+
+            }
+        }
+    }, modifier = Modifier.padding(20.dp))
+}
