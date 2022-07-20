db63d56995543a937c921650bf3cdbda111e6e5e
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-21 01:48:44 +0200

    added a composable for dotted lines

---
+++ app/src/main/java/de/pascalkuehnold/triviaapp/components/DrawDottedLine.kt
@@ -0,0 +1,27 @@
+package de.pascalkuehnold.triviaapp.screens
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.unit.dp
+import de.pascalkuehnold.triviaapp.util.AppColors
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(x = 0f, y = 0f),
+            end = Offset(size.width, y = 0f),
+            pathEffect = pathEffect
+        )
+    }
+}
