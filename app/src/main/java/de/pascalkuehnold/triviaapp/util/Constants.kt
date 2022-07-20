d571a591a9855b307455de177f924f365c8ed56d
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-21 01:47:30 +0200

    added constants object for accessing base url for trivia json

---
+++ app/src/main/java/de/pascalkuehnold/triviaapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package de.pascalkuehnold.triviaapp.util
+
+
+/**
+ * Created by Pascal Kühnold on 7/18/2022.
+ */
+object Constants {
+    //https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/world.json
+    const val BASE_URL = "https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/"
+}
