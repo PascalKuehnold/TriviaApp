e7b3ecc084fbecc502b840e2e8c19c60b30ca650
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-21 01:44:51 +0200

    added a color palette

---
+++ app/src/main/java/de/pascalkuehnold/triviaapp/util/AppColors.kt
@@ -0,0 +1,21 @@
+package de.pascalkuehnold.triviaapp.util
+
+import androidx.compose.ui.graphics.Color
+
+
+/**
+ * Created by Pascal Kühnold on 7/20/2022.
+ */
+object AppColors {
+
+    val mLightGray = Color(0xffb4b9cd)
+    val mBlack = Color(0xff060814)
+    val mOffWhite = Color(0xffeceeef)
+    val mLightPurple = Color(0xff545a75)
+    val mDarkPurple = Color(0xff262C49)
+    val mOffDarkPurple = Color(0xff22496B)
+    val mLightBlue = Color(0xff127eea)
+    val mBrown = Color(0xff7f6749)
+    val mBlue = Color(0xff4b5f9e)
+
+}
